5bde04aed3e155f79b5f6900d4e8885a96eccff6
DANNJUGUNA <ndan30071@gmail.com>
2022-11-28 10:55:50 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Project title
+week 1 code challenge
+## Project Description
+This project includes solutions for  three toy projects.
+1.
--- Student_Grade_Generator/index.js
+++ Student_Grade_Generator/index.js
@@ -1,28 +1,42 @@
+const prompt=require("prompt-sync")();
 
-let mark;
 function grade(){
+    //prompt the user to input marks
+  let marks=prompt("Enter marks:");
+    //parrsing the input to a variable
+    let mark=marks;
     //checking if the marks input is valid
-    if (mark>=0 && mark<=100){
+    
+    if(mark!==null){
       //when marks is greater than 79 grade is A
-        if(mark>79){
-            console.log('A');
+        if((mark>79 &&mark<=100)){
+            console.log('Grade: A');
         }
         //when marks is greater than 59 and less than or equal to 79 grade is B
         else if(mark>59 && mark<=79){
-            console.log("B")
+            console.log("Grade: B")
         }
-        //when marks is greater than 49 and less than or equal to 59 grade is B
+        //when marks is greater than 49 and less than or equal to 59 grade is C
         else if(mark>49 && mark<=59){
-            console.log("C")
+            console.log("Grade: C")
         }
+        //when marks is greater than 39 and less than or equal to 49 grade is D
         else if(mark>39 && mark<=49){
-            console.log("D")
-        }else{
-            console.log('E')
+            console.log("Grade: D")
         }
-    }
-    else{
-        console.log("marks out of range")
+        //when marks is greater than 0 and less than or equal to 39 grade is E
+        else if(mark<=30 &&mark>-1){
+            console.log('Grade: E')
+        }
+        // marks less than 0 and greater than 100
+        else{
+            console.log("marks out of range")
+        }
+    }else if(mark===nill){
+        console.log("enter ")
     }
 }
+    
+    
+
 grade();
